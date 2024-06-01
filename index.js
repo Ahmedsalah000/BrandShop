e4b421a125d7ac50980880435e0f23bbff4712da
Ahmed Aboshendy <a7meddon123@gmail.com>
2024-06-02 01:09:29 +0300

    prod3

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(express.json());
 // app.options('*', cors());
 
 // Parse URL-encoded bodies
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+// app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(express.static(path.join(__dirname, 'uploads')));
 
 if (process.env.NODE_ENV === 'development') {
