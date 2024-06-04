d673e77f4b0a3a7e5d5470437b9afb3db3de7e87
Ahmed Aboshendy <a7meddon123@gmail.com>
2024-06-05 00:13:21 +0300

    index

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(express.json());
 // app.options('*', cors());
 
 // Parse URL-encoded bodies
-// app.use(express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
