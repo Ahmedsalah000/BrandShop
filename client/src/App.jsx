c1f8550508ba0557d4312c592ab0354a8ff5f517
Ahmed Aboshendy <a7meddon123@gmail.com>
2024-09-10 18:44:02 +0300

    adding redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,6 +23,12 @@ import UserAllAddresPage from './Page/User/UserAllAddresPage';
 import UserAddAddressPage from './Page/User/UserAddAddressPage';
 import UserEditAddressPage from './Page/User/UserEditAddressPage';
 import UserProfilePage from "./Page/User/UserProfilePage";
+import AdminEditProductsPage from './Page/Admin/AdminEditProductsPage';
+import ForgetPasswordPage from "./Page/Auth/ForgetPasswordPage";
+import VerifyPasswordPage from "./Page/Auth/VerifyPasswordPage";
+import RsetPasswordPage from "./Page/Auth/ResetPasswordPage";
+import AdminAddCouponPage from "./Page/Admin/AdminAddCouponPage";
+import AdminEditCouponPage from './Page/Admin/AdminEditCouponPage';
 function App() {
   return (
     <div className="font" >
@@ -45,13 +51,19 @@ function App() {
           <Route path="/admin/addcategory" element={<AdminAddCategoryPage />} />
           <Route path="/admin/addsubcategory" element={<AdminAddSubCategoryPage />} />
           <Route path="/admin/addproduct" element={<AdminAddProductsPage />} />
+          <Route path="/admin/addcoupon" element={<AdminAddCouponPage />} />
+          <Route path="/admin/editcoupon/:id" element={<AdminEditCouponPage />} />
           <Route path="/user/allorders" element={<UserAllOrdersPage />} />
           <Route path="/user/favoriteproducts" element={<UserFavoriteProductsPage />} />
           <Route path="/user/addresses" element={<UserAllAddresPage />} />
           <Route path="/user/add-address" element={<UserAddAddressPage />} />
-          <Route path="/user/edit-address" element={<UserEditAddressPage />} />  
-          <Route path="/user/profile" element={<UserProfilePage />} />  
-          
+          <Route path="/user/edit-address/:id" element={<UserEditAddressPage />} />
+          <Route path="/user/profile" element={<UserProfilePage />} />
+          <Route path="/admin/editproduct/:id" element={<AdminEditProductsPage />} />
+          <Route path="/user/forget-password" element={<ForgetPasswordPage />} />
+          <Route path="/user/verify-code" element={<VerifyPasswordPage />} />
+          <Route path="/user/reset-password" element={<RsetPasswordPage />} />
+
         </Routes>
       </BrowserRouter>
       <Footer />
